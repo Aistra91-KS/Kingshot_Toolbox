96ed22066f13a769ccf8e373bec720490b813c19
Aistra91-KS <paulclavier77@gmail.com>
2026-06-11 20:05:14 +0200

    Create storage-keys.js

---
+++ js/js/storage-keys.js
@@ -0,0 +1,11 @@
+// Source unique des clés localStorage métier
+const STORAGE_KEYS = {
+    caserneHeroes:  'caserne_user_heroes',
+    caserneFilters: 'caserne_filters',
+    masters:        'masters_user_data',
+    researchDb:     'research_calc_db_v9',
+    researchInputs: 'research_calc_inputs_v9',
+    beartrap:       'beartrap_data',
+    truegold:       'tg_calc_data_v3'
+};
+window.STORAGE_KEYS = STORAGE_KEYS;
