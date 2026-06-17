0444d15d393658e7848929e83e584cce8c2e7d97
Aistra91-KS <paulclavier77@gmail.com>
2026-06-17 19:12:36 +0200

    Update truegold_script.js

--- js/truegold_script.js
+++ js/truegold_script.js
@@ -247,7 +247,7 @@ function renderBuildings() {
     const emojis = {
         "Town Center": iconSvg('landmark',16), "Embassy": iconSvg('handshake',16), "Infirmary": iconSvg('heart-pulse',16),
         "Command Center": iconSvg('star',16), "War Academy": iconSvg('swords',16),
-        "Barracks": iconSvg('shield',16), "Stable": '<svg class="ic" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M5 22h14a1 1 0 0 0 0-2h-1.18l-.74-7.43A7 7 0 0 0 12.6 6.2l-2.05-1.32a1 1 0 0 0-1.42 1.3l.6 1.04-1 .86a1 1 0 0 0-.2 1.26l.7 1.2-.3.18a6 6 0 0 0-2.9 5.9L6 20H5a1 1 0 0 0 0 2Z"/></svg>', "Range": iconSvg('target',16)
+        "Barracks": iconSvg('shield',16), "Stable": '<svg class="ic" width="16" height="16" viewBox="0 0 14 14" fill="none" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M2.42 11.256a.97.97 0 0 1 .92-.662h7.321c.417 0 .787.267.92.662l.747 2.244H1.672z"/><path d="M3.572 10.594c.158-.83 1.306-1.783 2.117-2.418c1.945-1.522 1.765-2.824 1.447-3.177L5.193 6.11a1.23 1.23 0 0 1-1.437-.154v0a1.23 1.23 0 0 1-.237-1.543L4.983 1.93L4.42.658c.93-.33 3.501-.155 4.518.635c1.27.989 2.894 2.489 1.553 9.3"/><path d="M7.773 3.971a1.9 1.9 0 0 1-.631 1.03"/></svg>', "Range": iconSvg('target',16)
     };
 
     buildingsState.forEach((b, index) => {
